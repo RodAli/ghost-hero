ee248f117cb1c4077ac5f5b2ac3a2e488fa3f2a9
Rod Ali Mazloomi <rod.mazloomi@hotmail.com>
2025-03-14 20:27:29 -0400

    Added main menu button to the game over screen

--- src/scenes/Pause.ts
+++ src/scenes/Pause.ts
@@ -1,25 +1,65 @@
-import { font, size } from '../constants';
+import { colors, font, size } from '../constants';
 
 export class Pause extends Phaser.Scene {
     constructor() {
         super('Pause');
     }
 
-    create() {
-        this.add.text(size.width / 2, size.height / 3, "Game over!", {
+    createTitleText() {
+        this.add.text(size.width / 2, size.height / 4, "Game over!", {
             fontFamily: font,
             fontSize: "100px",
             color: "#000000"
-        }).setOrigin(0.5, 0.5);
-        this.add.text(size.width / 2, (size.height / 3) * 2, "Press Enter key to restart", {
+        }).setOrigin(0.5);
+    }
+
+    createPromptText() {
+        this.add.text(size.width / 2, size.height / 2, "Press Enter or Space key to restart", {
             font: "50px Arial",
             color: "#000000"
-        }).setOrigin(0.5, 0.5);
+        }).setOrigin(0.5);
+    }
+
+    setupKeyHandlers() {
         
-        // Add Enter key handler to resume the game
-        this.input.keyboard?.on('keydown-ENTER', () => {
+        const handler = () => {
             this.scene.stop('Pause');
             this.scene.launch('Game');
+        }
+        this.input.keyboard?.on('keydown-ENTER', handler);
+        this.input.keyboard?.on('keydown-SPACE', handler);
+    }
+
+    createMainMenuButton() {
+        const button = this.add.text(size.width / 2, size.height * (3 / 4), 'Main Menu', {
+            fontFamily: 'Arial',
+            fontSize: '32px',
+            color: '#ffffff',
+            align: 'center',
+            fixedWidth: 260,
+            backgroundColor: colors.buttonColor,
+        }).setPadding(32).setOrigin(0.5);
+
+        button.setInteractive({ useHandCursor: true });
+
+        button.on('pointerover', () => {
+            button.setBackgroundColor(colors.buttonColorHover);
         });
+
+        button.on('pointerout', () => {
+            button.setBackgroundColor(colors.buttonColor);
+        });
+
+        button.on("pointerdown", () => {
+            this.scene.stop('Pause');
+            this.scene.launch('Menu');
+        })
+    }
+
+    create() {
+        this.createTitleText();
+        this.createPromptText();
+        this.createMainMenuButton();
+        this.setupKeyHandlers();
     }
 }
